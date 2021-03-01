cfc620da3d65212826da951b75cf205e2e54b976
Pete Williams <pete.williams82@gmail.com>
2021-03-01 13:40:01 +0000

    updates for testing

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,17 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
 // Add your routes here - above the module.exports line
 
-module.exports = router
+router.get('*/prototype-admin/view-data', function(req, res){
+
+	querystring = '';
+	for ( var key in req.session.data )
+	querystring += key +'=' + req.session.data[key] + '&';
+
+  res.render('prototype-admin/view-data', { data: JSON.stringify( req.session, null, 2), querystring: querystring } )
+});
+
+
+module.exports = router;
+
