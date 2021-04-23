7a33c5a70e4434fa64fd4cd122b101772a32935d
Pete Williams <pete.williams82@gmail.com>
2021-04-23 17:08:44 +0100

    adding location data tab

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Approve organisations',
+  serviceName: 'Manage data and organisation approvals',
 
   // Default port that prototype runs on
   port: '3000',
